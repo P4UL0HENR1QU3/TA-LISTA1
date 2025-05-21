ce361061df344ab1ff0e5af356f56b989c5132c2
Paulo Henrique <paulohenriqueooliveira.2021@gmail.com>
2025-05-21 01:15:28 -0300

    Finalizando o Exercicio 2, logo finalizando a lista

--- Calculando N numeros/2.Usando comprehensions/funcoes.py
+++ Calculando N numeros/2.Usando comprehensions/funcoes.py
@@ -7,51 +7,26 @@ def somar_lista(numeros):
 
 def multiplicar_lista(numeros):
     #infelizmente nao achei outra maneira e quis fazer parecido com o de cima
-    multi = prod([num for num in numeros])
-    return multi
     # multi = prod(numeros)
     # return multi
+    multi = prod([num for num in numeros])
+    return multi
 
 def duplicados_lista(numeros):
-    verificar = []
-    repetidos = set()
-
-    for i in numeros:
-        if i not in verificar:
-            verificar.append(i)
-        else:
-            repetidos.add(i)
-
+    #Talvez desse para melhorar o exercicio 1 e usei set para nao repetir o numero repetido varias vezes
+    repetidos = set([num for num in numeros if numeros.count(num) > 1])
     return list(repetidos)
 
 def impares_lista(numeros):
-    impares = []
-    for i in numeros:
-        if i % 2 != 0 and i not in impares:
-            impares.append(i)
-
+    impares = set([num for num in numeros if num % 2 != 0])
     return list(impares)
 
 def pares_lista(numeros):
-    pares = []
-    for i in numeros:
-        if i % 2 == 0 and i not in pares:
-            pares.append(i)
-        
+    pares = set([num for num in numeros if num % 2 == 0])
     return list(pares)
 
 def primos_lista(numeros):
-    primos = []
-
-    for numero in numeros:
-        primo = 1
-
-        for i in range(2, numero):
-            if numero % i == 0:
-                primo = 0
-                break
-
-        if primo == 1 and numero > 1 and numero not in primos:
-            primos.append(numero)
-
+    primos = set([num for num in numeros if num > 1 and all(num % i != 0 for i in range(2, num))])                      
     return list(primos)
+
+   
--- Calculando N numeros/2.Usando comprehensions/main.py
+++ Calculando N numeros/2.Usando comprehensions/main.py
@@ -14,60 +14,27 @@ def exibir_resultados(numeros):
     print(f"Exibicao dos numeros inteiros pares: {exibir_pares}")
     print(f"Exibicao dos numeros primos: {exibir_primos}")
 
+def exibir_pergunta(numeros=None):
+    if numeros is None:
+        numeros = []
+
+    try:
+        numero = int(input("Digite um número inteiro (0 para parar): "))
+        if numero == 0:
+            print(f"Numeros digitados: {numeros}")
+            exibir_resultados(numeros)
+            numeros.append(numero)
+            return numeros
+        else:
+            numeros.append(numero)
+            return exibir_pergunta(numeros) 
+    except ValueError:
+        print("Coloque um valor inteiro")
+        return exibir_pergunta(numeros)
+
 def main():
 
-    numeros = []
-
-    while True:
-        try:
-            numero = int(input("Digite um número inteiro (0 para parar): "))
-            if numero == 0:
-                break
-            numeros.append(numero)
-        except ValueError:
-            print("Coloque um valor inteiro")
-
-    print(f"Numeros digitados: {numeros}")
-    exibir_resultados(numeros)
+    exibir_pergunta()
 
 if __name__ == "__main__":
     main()
-
-
-# from funcoes import (somar_lista, multiplicar_lista, duplicados_lista, impares_lista, pares_lista, primos_lista)
-
-# def exibir_resultados(numeros):
-#     exibir_soma = somar_lista(numeros)
-#     exibir_multi = multiplicar_lista(numeros)
-#     exibir_duplicados = duplicados_lista(numeros)
-#     exibir_impares = impares_lista(numeros)
-#     exibir_pares = pares_lista(numeros)
-#     exibir_primos = primos_lista(numeros)
-#     print(f"Calculo da soma dos numeros inteiros: {exibir_soma}")
-#     print(f"Calculo da multiplicacao dos numeros inteiros: {exibir_multi}")
-#     print(f"Exibicao dos numeros inteiros duplicados/repetidos: {exibir_duplicados}")
-#     print(f"Exibicao dos numeros inteiros impares: {exibir_impares}")
-#     print(f"Exibicao dos numeros inteiros pares: {exibir_pares}")
-#     print(f"Exibicao dos numeros primos: {exibir_primos}")
-
-# def main():
-
-#     numeros = []
-
-#     numero = int(input("Digite um número inteiro (0 para parar): "))
-
-#     if len(numeros) == 1:
-#         numeros = input("Coloque mais um número para que fique mais interessante: ")
-        
-#         numero = int(input("Digite um número inteiro (0 para parar): "))
-#         if numero == 0:
-#             break
-#         numeros.append(numero)
-
-#     if numeros != 0:
-
-#     print(f"Numeros digitados: {numeros}")
-#     exibir_resultados(numeros)
-
-# if __name__ == "__main__":
-#     main()
